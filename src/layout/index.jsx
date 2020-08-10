5cbba786ece93af45804c615cb0ee6311c3788e1
guilloutran <will@guilloutran.com>
2020-08-10 23:49:46 +0200

    Setting Up Algolia

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import config from "../../data/SiteConfig";
 import "./index.css";
+import ReactDOM from 'react-dom';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { title } from "case";
+//import SearchBox from "../components/SearchBox/SearchBox.js";
+
+const searchClient = algoliasearch(
+  'latency',
+  '768a7c6eff9f904e6e63bb92d549e087'
+);
 
 export default class MainLayout extends React.Component {
   render() {
@@ -13,7 +23,17 @@ export default class MainLayout extends React.Component {
           <html lang="en" />
         </Helmet>
         {children}
+        <div>
+          <InstantSearch
+            indexName="prod_GuillouTran"
+            searchClient={title}
+          >
+            <SearchBox />
+            <Hits />
+          </InstantSearch>
+        </div>
       </div>
+      
     );
   }
 }
